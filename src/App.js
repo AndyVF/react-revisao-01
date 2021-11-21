fb35816277f8971d3d7e7106cab4cf244ea5469b
Anderson <andersonfinho@hotmail.com>
2021-11-21 20:01:48 -0300

    feito

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Galeria from "../src/Components/Galeria"
+
+function App() {
+  return (
+    <div className="App">
+      <Galeria/>
+    </div>
+  );
+}
+
+export default App;
